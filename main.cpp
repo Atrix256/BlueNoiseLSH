c664d4af7c536e813e1f255dc503241d4c791ff4
Alan Wolfe <alan.wolfe@gmail.com>
2018-10-05 20:53:41 -0700

    some good progress. need to do blue noise. need to fix uniform. need to figure out in general how to balance angle vs offset for things like blue noise, uniform and golden ratio.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 // Configurable settings
 #define DIMENSION()    2      // dimensionality of the data points
-#define NUMPOINTS()    100    // the number of randomly generated (white noise) data points
+#define NUMPOINTS()    1000   // the number of randomly generated (white noise) data points
 #define HASHCOUNT()    4      // how many hashes are done for each point
 #define MINHASHCOUNT() 0      // the minimum number of hash collisions that must occur for a point to be considered at all close
-#define POINTDOMAIN()  10     // the coordinates go from - this value to + this value
+#define POINTDOMAIN()  5      // the coordinates go from - this value to + this value
 
-#define IMAGEWIDTH() 500
-#define IMAGEHEIGHT() 500
+#define IMAGESIZE() 500       // the size of the image - width and height both.
 
 // TODO: tune HASHCOUNT() and the other parameters!
 
@@ -33,6 +32,7 @@ typedef uint8_t uint8;
 enum class PointID : uint32 {};
 
 static const float c_pi = 3.14159265359f;
+static const float c_goldenRatioConjugate = 0.61803398875f;
 
 // -------------------------------------------------------------------------------
 
@@ -245,6 +245,32 @@ void GeneratePointHashDatas_Uniform(std::array<PointHashData, HASHCOUNT()>& hash
 
 // -------------------------------------------------------------------------------
 
+void GeneratePointHashDatas_GoldenRatio(std::array<PointHashData, HASHCOUNT()>& hashDatas)
+{
+    static_assert(DIMENSION() == 2, "This function only works with 2d rotation matrices");
+
+    // TODO: how to deal with offset? do we need the generalized golden ratio and need to square it?
+
+    for (int i = 0; i < HASHCOUNT(); ++i)
+    {
+        PointHashData& p = hashDatas[i];
+
+        // TODO: should we choose between 0 and pi, instead of 2 pi?
+        // choose an angle from 0 to 180 degrees, because angle > 180 degrees is redundant.
+        float angle = std::fmodf(float(i) * c_goldenRatioConjugate, 1.0f) * 2.0f * c_pi;
+
+        float cosTheta = std::cosf(angle);
+        float sinTheta = std::sinf(angle);
+
+        p.rotation = { cosTheta, -sinTheta,
+                       sinTheta,  cosTheta };
+
+        p.offsetX = 0.0f;// percentY;
+    }
+}
+
+// -------------------------------------------------------------------------------
+
 float SmoothStep(float value, float min, float max)
 {
     float x = (value - min) / (max - min);
@@ -313,7 +339,7 @@ void DrawHashBuckets(std::vector<uint8>& pixels, int imageWidth, int imageHeight
             float distance = 0.5f - fabsf(distX - 0.5f);
 
             // draw the hash bucket lines
-            float alpha = SmoothStep(distance * float(POINTDOMAIN()) * 2.0f, 2.0f, 0.0f);
+            float alpha = SmoothStep(distance, 4.0f * float(POINTDOMAIN()) / float(IMAGESIZE()), 0.0f);
             pixel[0] = Lerp(pixel[0], R, alpha);
             pixel[1] = Lerp(pixel[1], G, alpha);
             pixel[2] = Lerp(pixel[2], B, alpha);
@@ -331,21 +357,19 @@ void ReportQueryAsImage(const LHS& lhs, const TPoint& queryPoint, const std::uno
     char fileName[256];
     sprintf_s(fileName, "out/%s.png", name);
 
-    int width = IMAGEWIDTH();
-    int height = IMAGEHEIGHT();
-    int circleRadius = int(float(width) / 100.0f);
-    int colorCircleRadius = std::min(int(float(width) / 100.0f * 0.9f), circleRadius - 1);
+    int circleRadius = int(float(IMAGESIZE()) / 100.0f);
+    int colorCircleRadius = std::min(int(float(IMAGESIZE()) / 100.0f * 0.9f), circleRadius - 1);
 
     // create the image and fill it with white
     std::vector<uint8> pixels;
-    pixels.resize(width*height * 4);
+    pixels.resize(IMAGESIZE()*IMAGESIZE() * 4);
     std::fill(pixels.begin(), pixels.end(), 255);
 
     // draw the hash buckets
     for (int hashIndex = 0; hashIndex < HASHCOUNT(); ++hashIndex)
     {
         const PointHashData& pointHashData = lhs.GetPointHashData(hashIndex);
-        DrawHashBuckets(pixels, width, height, float(width) * 0.5f / float(POINTDOMAIN()), pointHashData, 192, 192, 192);
+        DrawHashBuckets(pixels, IMAGESIZE(), IMAGESIZE(), float(IMAGESIZE()) * 0.5f / float(POINTDOMAIN()), pointHashData, 192, 192, 192);
     }
 
     // draw all the points as black dots
@@ -357,10 +381,10 @@ void ReportQueryAsImage(const LHS& lhs, const TPoint& queryPoint, const std::uno
             f += 0.5f;
         }
 
-        int x = int(p[0] * float(width - 1) + 0.5f);
-        int y = int(p[1] * float(height - 1) + 0.5f);
+        int x = int(p[0] * float(IMAGESIZE() - 1) + 0.5f);
+        int y = int(p[1] * float(IMAGESIZE() - 1) + 0.5f);
 
-        DrawCircle(pixels, width, height, x, y, circleRadius, 0, 0, 0);
+        DrawCircle(pixels, IMAGESIZE(), IMAGESIZE(), x, y, circleRadius, 0, 0, 0);
     }
 
     // draw the results based on their match count
@@ -377,13 +401,13 @@ void ReportQueryAsImage(const LHS& lhs, const TPoint& queryPoint, const std::uno
             f += 0.5f;
         }
 
-        int x = int(p[0] * float(width - 1) + 0.5f);
-        int y = int(p[1] * float(height - 1) + 0.5f);
+        int x = int(p[0] * float(IMAGESIZE() - 1) + 0.5f);
+        int y = int(p[1] * float(IMAGESIZE() - 1) + 0.5f);
 
         float percentMatched = (float(it.second) - float(MINHASHCOUNT())) / float(HASHCOUNT() - MINHASHCOUNT());
         uint8 color = uint8(percentMatched * 255.0f);
 
-        DrawCircle(pixels, width, height, x, y, colorCircleRadius, 255, color, 0);
+        DrawCircle(pixels, IMAGESIZE(), IMAGESIZE(), x, y, colorCircleRadius, 255, color, 0);
     }
 
     // draw the query point
@@ -394,13 +418,13 @@ void ReportQueryAsImage(const LHS& lhs, const TPoint& queryPoint, const std::uno
             f /= (2.0f * float(POINTDOMAIN()));
             f += 0.5f;
         }
-        int x = int(p[0] * float(width - 1) + 0.5f);
-        int y = int(p[1] * float(height - 1) + 0.5f);
+        int x = int(p[0] * float(IMAGESIZE() - 1) + 0.5f);
+        int y = int(p[1] * float(IMAGESIZE() - 1) + 0.5f);
 
-        DrawCircle(pixels, width, height, x, y, circleRadius, 0, 0, 255);
+        DrawCircle(pixels, IMAGESIZE(), IMAGESIZE(), x, y, circleRadius, 0, 0, 255);
     }
 
-    stbi_write_png(fileName, width, height, 4, pixels.data(), 0);
+    stbi_write_png(fileName, IMAGESIZE(), IMAGESIZE(), 4, pixels.data(), 0);
 }
 
 // -------------------------------------------------------------------------------
@@ -450,8 +474,9 @@ int main(int argc, char** argv)
     }
 
     // add the points to the locality sensitive hashing
-    LHS lhs_white(points, GeneratePointHashDatas_WhiteNoise);
+    LHS lhs_whiteNoise(points, GeneratePointHashDatas_WhiteNoise);
     LHS lhs_uniform(points, GeneratePointHashDatas_Uniform);
+    LHS lhs_goldenRatio(points, GeneratePointHashDatas_GoldenRatio);
 
     // do some queries
     for (int i = 0; i < 1; ++i)
@@ -462,16 +487,15 @@ int main(int argc, char** argv)
 
         printfPoint("Query Point", queryPoint);
 
-        ReportQuery(lhs_white, queryPoint, points, "white");
+        ReportQuery(lhs_whiteNoise, queryPoint, points, "whiteNoise");
         ReportQuery(lhs_uniform, queryPoint, points, "uniform");
+        ReportQuery(lhs_goldenRatio, queryPoint, points, "goldenRatio");
     }
 
     // TODO: ground truth, how? maybe visually show points missed or something? could calculate std deviations as concentric rings.
 
     return 0;
 }
-
-// TODO: it seems like the grid lines may be wrong sometimes even with no x offset. check out the white noise screenshot checked in. It probably has to do with where it goes negative. the distance calculation is probably wrong...
 
 /*
 
@@ -507,7 +531,7 @@ Tests:
 
 
 Links:
-- inspiration: http://unboxresearch.com/articles/lsh_post1.html
+- inspiration: http://unboxresearch.com/articles/lsh_post1.html   aka http://unboxresearch.com/articles/lsh_post1.html
 * another way to do it: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.215.7690&rep=rep1&type=pdf
  * make regular cells of any shape. store a list at each cell corner which has each point that is in a cell that uses that corner.
  * talks about simplices fighting the curse of dimensionality.
